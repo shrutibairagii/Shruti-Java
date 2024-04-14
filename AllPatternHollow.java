2ec45e52bc58f975ac4194de6b4dc75d55720852
shrutibairagii <130243909+shrutibairagii@users.noreply.github.com>
2024-04-14 23:57:00 +0530

    java program

---
+++ AllPatternHollow.java
@@ -0,0 +1,185 @@
+class AllPatternHollow
+{
+	public static void main(String[] args) 
+	{
+// 		for (int i =1;i<=5 ;i++ ) 
+// 		{
+// 			for (int j =1;j<=i ;j++ ) 
+// 			{
+// 				if (j==1 || i==j || i==5) 
+// 				{
+// 					System.out.print("* ");	
+// 				}			
+// 				else 
+// 				{
+// 					System.out.print("  ");
+// 				}
+// 			}		
+// 			System.out.println();
+// 		}
+// *
+// * *
+// *   *
+// *     *
+// * * * * *
+
+
+// 		for (int i=5;i>=1 ;i-- ) 
+// 		{
+// 			for (int j=1;j<=i ;j++ ) 
+// 			{
+// 				if (j==1||i==5||i==j) 
+// 				{
+// 					System.out.print("* ");				
+// 				}
+// 				else 
+// 				{
+// 					System.out.print("  ");
+// 				}			
+// 			}
+// 			System.out.println();		
+// 		}
+// * * * * *
+// *     *
+// *   *
+// * *
+// *	
+
+// 		for (int i=1;i<=5 ;i++ ) 
+// 		{
+// 			for (int j =4;j>=i ;j-- ) 
+// 			{
+// 				System.out.print(" ");			
+// 			}	
+// 			for (int j=1;j<=i ;j++ ) 
+// 			{
+// 				if (j==1 || i==5 || j==i )
+// 				{
+// 					System.out.print("*");	
+// 				}
+// 				else 
+// 				{
+// 					System.out.print(" ");
+// 				}	
+					
+// 			}
+// 			System.out.println();
+// 		}
+//     *
+//    **
+//   * *
+//  *  *
+// *****
+
+// 		for (int i=5;i>=1 ;i-- ) 
+// 		{
+// 			for (int j=5;j>i ;j-- ) 
+// 			{
+// 				System.out.print(" ");		
+// 		    }	
+// 		    for (int j=1;j<=i ;j++ ) 
+// 		    {
+		    	
+// 		    	if (j==1||i==5||i==j) 
+// 		    	{
+// 		    		System.out.print("*");		
+// 		    	}	
+// 		    	else 
+// 		    	{
+// 		    		System.out.print(" ");
+// 		    	}
+// 		    }
+// 		    System.out.println();
+// 		}
+// *****
+//  *  *
+//   * *
+//    **
+//     *
+
+		for (int i=1;i<=5 ;i++ ) 
+		{
+			for (int j=4;j>=i ;j-- ) 
+			{
+				System.out.print(" ");		
+			}
+			for (int j=1;j<=i ;j++ ) 
+			{
+				if (i==5||j==1 ||i==2||((i==3)||(j==5)))
+				{
+					System.out.print("* ");			
+				}		
+				else 
+				{
+					System.out.print(" ");
+				}
+
+			}	
+			System.out.println();
+		}
+
+
+
+		// for (int i=5;i>=1 ;i-- ) 
+		// {
+		// 	for (int j =5;j>i ;j-- ) 
+		// 	{
+		// 		System.out.print(" ");		
+		// 	}	
+		// 	for (int j =1;j<=i ;j++ ) 
+		// 	{
+		// 		if (i==1||j==5) {
+						
+		// 			}	
+		// 	}
+		// 	System.out.println();
+		// }
+
+
+
+// 			for (int i=1;i<=5 ;i++ ) 
+// 			{
+// 				for (int j=1;j<=i ;j++ ) 
+// 				{
+// 					if (j==1||i==j) 
+// 					{
+// 						System.out.print("* ");		
+						
+// 					}	
+// 					else 
+// 					{
+// 						System.out.print("  ");
+// 					}
+// 				}
+// 			System.out.println();		
+// 			}	
+// 			for (int i =4;i>=1 ;i-- ) 
+// 			{
+// 			for (int j =1;j<=i ;j++ ) 
+// 			{
+// 				if (j==1||i==j)
+// 				{
+// 					System.out.print("* ");	
+// 				}	
+// 				else 
+// 				{
+// 					System.out.print("  ");
+// 				}
+// 			}
+// 			System.out.println();
+// 			}
+
+// *
+// * *
+// *   *
+// *     *
+// *       *
+// *     *
+// *   *
+// * *
+// *
+
+
+
+	}
+}
